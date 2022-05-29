4b5b775f5685140a314e7c46bf6f81d73899ff8b
RUBIKA B <104882621+RUBIKA132@users.noreply.github.com>
2022-05-29 16:03:44 +0530

    Add files via upload

---
+++ Week3.Day2/Outofbounds.java
@@ -0,0 +1,17 @@
+package week3.Day2;
+
+public class Outofbounds {
+
+	public Outofbounds() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//Assigning the values of Array
+		int[]arr=new int[4];
+		
+
+	}
+
+}
