1dacf78647f39e0e500b4c6db132cdb4d460c070
RUBIKA B <104882621+RUBIKA132@users.noreply.github.com>
2022-05-29 16:08:54 +0530

    Add files via upload

---
+++ Week3.Day2/Reverse.java
@@ -0,0 +1,15 @@
+package week3.Day2;
+
+public class Reverse {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String str = "testleaf" ;
+		char[] charArray = str.toCharArray();
+		for(int i=charArray.length-1;i>=0;i--) {
+		System.out.println(charArray[i]);
+
+	}
+
+}
+}
